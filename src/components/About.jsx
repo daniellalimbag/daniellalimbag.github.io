dcdaefe4a5356727d2eab1b9ffe17c4bc616dbae
Daniella Limbag <92845499+daniellalimbag@users.noreply.github.com>
2025-04-28 10:28:03 +0800

    removed download resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import LaptopContainer from "./LaptopContainer";
-import { RiFileDownloadLine } from "react-icons/ri";
+import { IoIosDocument } from "react-icons/io";
 
 const About = () => (
   <section className="w-full min-h-[300px] h-auto lg:h-[60vw] lg:max-h-[70vh] mx-0 relative flex flex-col lg:flex-row items-center justify-center bg-[color:var(--bg)] rounded-none shadow-none mt-0 border-t border-t-[color:var(--text)] px-3 sm:px-4 md:px-8 lg:px-12 xl:px-25 2xl:px-40 [@media(min-width:3840px)]:px-[18vw]" id="about">
@@ -11,10 +11,11 @@ const About = () => (
       </p>
       <a
         href="/RESUME_LIMBAG_DANIELLA.pdf"
-        download
+        target="_blank"
+        rel="noopener noreferrer"
         className="mt-3 sm:mt-4 [@media(min-width:3840px)]:mt-10 inline-block px-4 py-2 [@media(min-width:3840px)]:px-8 [@media(min-width:3840px)]:py-4 bg-[color:var(--violet)] hover:bg-[color:var(--green)] text-white font-semibold rounded-lg shadow-md transition-colors duration-200 text-sm sm:text-base [@media(min-width:3840px)]:text-2xl flex items-center gap-2"
       >
-        <RiFileDownloadLine className="text-base sm:text-lg [@media(min-width:3840px)]:text-3xl" /> Resume
+        <IoIosDocument className="text-base sm:text-lg [@media(min-width:3840px)]:text-3xl" /> Resume
       </a>
     </div>
     <div className="w-full lg:w-1/2 flex-1 flex flex-col items-center justify-center h-48 sm:h-56 md:h-64 lg:h-64 xl:h-72 2xl:h-80 [@media(min-width:3840px)]:h-[32rem] max-w-none pb-4 sm:pb-6 lg:pb-0 pt-6 sm:pt-8 lg:pt-8 mx-auto">
